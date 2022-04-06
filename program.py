3f4aeabff988d58c54924a932ac464bf2441c703
usk-25 <yu.o.developer@gmail.com>
2022-04-06 23:26:56 +0900

    Revert "DB機能追加"
    This reverts commit 6527ee56faea92e3eda3ddcdbbeadbf5f61a629c.

--- program.py
+++ program.py
@@ -1,27 +1,10 @@
-import sqlite3
-from flask import Flask, render_template, request, g
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
 
-def get_db():
-    if 'db' not in g:
-        g.db = sqlite3.connect('my_flask_app1.db')
-        return g.db
-
-
 @app.route('/')
 def index():
-
-    # アプリケーション起動時になければテーブルを作成する。
-    con = get_db()
-    cur = con.execute(
-        "SELECT COUNT(*) FROM sqlite_master WHERE TYPE='table' AND name='todolist';")
-    for row in cur:
-        if row[0] == 0:
-            cur.execute("CREATE TABLE todolist(ID INTEGER PRIMARY KEY AUTOINCREMENT, TODO STRING(255), LIMITDATE DATETIME, CREATED_AT DATETIME NOT NULL DEFAULT 'DATETIME.NOW', UPDATED_AT DATETIME NOT NULL DEFAULT 'DATETIME.NOW');")
-
-    con.close()
     return render_template('index.html')
 
 
@@ -30,44 +13,16 @@ def add():
     if request.method == 'GET':
         return render_template('add.html')
     if request.method == 'POST':
-        toDo = request.form['toDoInput']
-        limitDate = request.form['toDoDate']
-
-        # DBコネクション
-        con = get_db()
-
-        # 登録処理
-        sql = "INSERT INTO todolist(TODO, LIMITDATE, CREATED_AT, UPDATED_AT) VALUES('{}','{}',datetime('now', 'localtime'), datetime('now', 'localtime'));".format(
-            toDo, limitDate)
-        con.execute(sql)
-        con.commit()
-
-        # データの再読み込み
-        cur = con.execute(
-            "SELECT ID, TODO, LIMITDATE FROM todolist ORDER BY ID")
-        data = cur.fetchall()
-        con.close()
-
-        return render_template('list.html', data=data)
+        req1 = request.form['toDoInput']
+        req2 = request.form['toDoDate']
+        return f'POST受け取りました: {req1} {req2}'
 
 
-@app.route('/list')
+@app.route('/list', methods=['GET', 'POST'])
 def list():
-    con = get_db()
-    cur = con.execute("SELECT ID, TODO, LIMITDATE FROM todolist ORDER BY ID")
-    data = cur.fetchall()
-    con.close()
-    return render_template('list.html', data=data)
+    if request.method == 'GET':
+        return render_template('list.html')
 
-
-# @app.route('/list/<int:id>')
-# def todo_delete(id):
-#     return render_template('list.html')
-
-
-# @app.route('/list/<int:id>/edit', methods=['POST'])
-# def todo_edit(id):
-#     return render_template('edit.html')
 
 # @app.route('/edit')
 # def add():
@@ -75,5 +30,5 @@ def list():
 
 
 if __name__ == '__main__':
-    DEBUG = True
+    app.debug = True
     app.run(host='localhost')
